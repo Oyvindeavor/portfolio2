7e2378e3ea61c8baa7d2fdf22baec68286849b3e
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2025-05-26 15:30:41 +0200

    Installed ShadCdn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <p>
       page
+      <Button variant={"outline"}> Click me </Button>
     </p>
   );
 }
